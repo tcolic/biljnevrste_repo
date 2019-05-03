391d06ae70dac83d929bd5fa881608d0ae042cba
tcolic <49404299+tcolic@users.noreply.github.com>
2019-05-03 20:41:30 +0200

    Created CRUD models

--- projekt/backend/biljnevrste/biljnevrsteapp/serializers.py
+++ projekt/backend/biljnevrste/biljnevrsteapp/serializers.py
@@ -1,8 +1,1 @@
 from rest_framework import serializers
-from .models import BiljneVrste
-
-class BiljneVrsteSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = BiljneVrste
-		fields = ('id','naziv','vrsta')
-	
